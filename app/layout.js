8195fe7678075dbc32235dadda3f719e500bea23
Oysturn Vas <oysturnxvas@gmail.com>
2024-06-25 15:38:02 +0530

    Added vercel Analytics and Speed Tester

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
 
@@ -12,6 +14,8 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Toaster position="top-center" />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
